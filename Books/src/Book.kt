71984f50c2a1e68ab5d3a39cf379ef55254dd81b
Khalid Elnagar <khaledmoon70163@gmail.com>
2019-03-16 13:31:46 +0200

    Practice: Constants

--- Books/src/Book.kt
+++ Books/src/Book.kt
@@ -1,9 +1,37 @@
+/*The Philosopher's Stone (1997)
+The Chamber of Secrets (1998)
+The Prisoner of Azkaban (1999)
+The Goblet of Fire (2000)
+The Order of the Phoenix (2003)
+The Half-Blood Prince (2005)
+The Deathly Hallows (2007)
+*/
+const val MAXIMUM_NUMBER_OF_BORROWED_BOOKS = 20
+
+fun main() {
+
+    println(createLibrary(createBook()))
+}
+
+object Constants {
+    const val BASE_URL = "library.com"
+}
+
 //inheritance
 open class Book(val title: String, val author: String) {
     private var currentPage = 1
+
+    companion object {
+        const val BASE_URL = "library.com"
+    }
+
     open fun readPage() {
         currentPage++
     }
+
+    fun printUrl() = println("$BASE_URL$title.html")
+    fun canBorrow(currentBorrowed: Int) = currentBorrowed < MAXIMUM_NUMBER_OF_BORROWED_BOOKS
+
 
     class eBook(var format: String = "text", title: String, author: String) : Book(title, author) {
         private var numberOfWords = 0
@@ -24,11 +52,6 @@ class BasicBooK(title: String, author: String, val year: String) : Book(title, a
     }
 }
 
-fun main() {
-
-    println(createLibrary(createBook()))
-}
-
 fun createLibrary(books: List<BasicBooK>): Map<String, List<BasicBooK>> {
     val allBooks: Map<String, List<BasicBooK>> = mapOf(books[0].author to books)
     println(allBooks.any { it.value.any { it.title == "The Goblet of Fire" } })
@@ -37,14 +60,7 @@ fun createLibrary(books: List<BasicBooK>): Map<String, List<BasicBooK>> {
     return allBooks
 }
 
-/*The Philosopher's Stone (1997)
-The Chamber of Secrets (1998)
-The Prisoner of Azkaban (1999)
-The Goblet of Fire (2000)
-The Order of the Phoenix (2003)
-The Half-Blood Prince (2005)
-The Deathly Hallows (2007)
-*/
+
 fun createBook(): List<BasicBooK> {
     val harryPotterBooks = listOf<BasicBooK>(
         BasicBooK(
